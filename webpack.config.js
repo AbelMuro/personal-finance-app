f33974d5d63ddc91b3ac3e332b989261b314b63b
abelmuro <abelmuro93@gmail.com>
2024-09-14 18:37:59 -0700

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,12 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');    //npm install copy-
 module.exports = {
 
     entry: './src/index.js',                  //this is where webpack will start its dependency graph, and will automatically figure out with modules depend on this entry point                  
-    output: {                                  //output is where our production code will be sent to               
+    output: {                                  //output is where our production code will be sent to               w
         path: path.join(__dirname, '/dist'),  //__dirname represents the current directory, /dist is the folder that will contain our production code
-        filename: 'bundle.js'                 //the bundled js file
+        filename: 'bundle.js',                 //the bundled js file
+        publicPath: '/',
+        clean: true,
+        assetModuleFilename: '[name][ext]',
     },
     plugins: [                      
         new HtmlWebpackPlugin({               //this plugin will help us generate the production html file in our /dist
