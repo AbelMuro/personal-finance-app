8cd325611e00330e3dfb777cee5927e3296dadfa
abelmuro <abelmuro93@gmail.com>
2024-10-27 17:02:37 -0700

    commit

--- src/Pages/Profile/Profile.js
+++ src/Pages/Profile/Profile.js
@@ -4,7 +4,6 @@ import {Outlet} from 'react-router-dom';
 import * as styles from './styles.module.css';
 
 function Profile(){
-    
     return(
         <main className={styles.container}>
             <NavigationBar/>
