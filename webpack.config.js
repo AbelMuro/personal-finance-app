988a65f36094529ac34b61db5f61e7589fd867e9
AbelMuro <abelmuro93@gmail.com>
2024-10-30 14:53:15 -0700

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -8,9 +8,6 @@ module.exports = {
     output: {                                  //output is where our production code will be sent to               w
         path: path.join(__dirname, '/dist'),  //__dirname represents the current directory, /dist is the folder that will contain our production code
         filename: 'bundle.js',                 //the bundled js file
-        publicPath: '/',
-        clean: true,
-        assetModuleFilename: '[name][ext]',
     },
     plugins: [                      
         new HtmlWebpackPlugin({               //this plugin will help us generate the production html file in our /dist
