851c8f1983a7935c4509891b5618d29b15d69b0a
abelmuro <abelmuro93@gmail.com>
2024-11-03 00:00:09 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ function App() {
                         <Route path='/profile/transactions' element={<Transactions/>}/>
                         <Route path='/profile/pots' element={<Pots/>}/>
                         <Route path='/profile/bills' element={<RecurringBills/>}/>
+                        <Route path='/profile/*' element={<NotFound/>}/>
                     </Route>
                     <Route path='*' element={<NotFound/>}/>
                 </Routes>
